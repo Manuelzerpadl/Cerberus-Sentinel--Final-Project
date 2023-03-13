bf44ee4a4f1697f9895fde54efff330e1137ad4a
manuelzerpadl <manuelzerpaa@gmail.com>
2023-03-13 23:02:52 +0100

    arreglando el script

--- code/ec2_python_script.py
+++ code/ec2_python_script.py
@@ -5,13 +5,10 @@ import datetime
 import os
 import base64
 
-AWS_ACCESS_KEY_ID = os.getenv("AWS_ACCESS_KEY_ID")
-AWS_SECRET_ACCESS_KEY = os.getenv("AWS_SECRET_ACCESS_KEY")
 
 
-s3 = boto3.client('s3',
-                  aws_access_key_id= AWS_ACCESS_KEY_ID,
-                  aws_secret_access_key= AWS_SECRET_ACCESS_KEY)
+
+s3 = boto3.client('s3')
 
 ec2 = boto3.resource('ec2')
 
